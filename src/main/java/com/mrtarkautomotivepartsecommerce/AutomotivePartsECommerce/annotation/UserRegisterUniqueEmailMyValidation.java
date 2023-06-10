e09e68069b8acc763dc59e883ee839170e3e4e2c
mrtark <murat_ark97@hotmail.com>
2023-06-10 16:22:23 +0300

    rev

--- src/main/java/com/mrtarkautomotivepartsecommerce/AutomotivePartsECommerce/annotation/UserRegisterUniqueEmailMyValidation.java
+++ src/main/java/com/mrtarkautomotivepartsecommerce/AutomotivePartsECommerce/annotation/UserRegisterUniqueEmailMyValidation.java
@@ -2,14 +2,23 @@ package com.mrtarkautomotivepartsecommerce.AutomotivePartsECommerce.annotation;
 
 import com.mrtarkautomotivepartsecommerce.AutomotivePartsECommerce.data.entity.UserRegisterEntity;
 import com.mrtarkautomotivepartsecommerce.AutomotivePartsECommerce.data.repository.IUserRegisterRepository;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class UserRegisterUniqueEmailMyValidation implements ConstraintValidator<UserRegisterUniqueEmailMy,String> {
-    final IUserRegisterRepository iUserRegisterRepository;
+    @Autowired
+    IUserRegisterRepository iUserRegisterRepository;
+
+    private String data="";
+    public String result(String requestData){
+        data=requestData;
+        return data;
+    }
     public boolean isValid(String email, ConstraintValidatorContext context){
         UserRegisterEntity userRegisterEntity = iUserRegisterRepository.findByEmail(email);
         if (userRegisterEntity!= null)
